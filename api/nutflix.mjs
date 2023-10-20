e6f0bff402161710e4389b63ee7d2b07b53b991e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-20 15:39:51 -0500

    Update nutflix.mjs

--- api/nutflix.mjs
+++ api/nutflix.mjs
@@ -8,41 +8,25 @@ globalThis.ligma=`<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍�
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
     width: 30vmin;
-"></img></center><br><center>TypeScript is blazingly fast </center><br><center>-Nutflix</center><br></div><div class=" col1"><center><img src="https://create.t3.gg/images/theo_300x300.webp" title="ligma balls" style="
+"></img></center><br><center><i>"TypeScript is blazingly fast"</i></center><br><center>-Nutflix</center><br></div><div class=" col1"><center><img src="https://create.t3.gg/images/theo_300x300.webp" title="ligma balls" style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
     width: 30vmin;
-"></img></center><br><center>The best crutches I've ever used </center><br><center>-Ligma</center><br></div><div class=" col1"><center><img src="https://pbs.twimg.com/profile_images/557940120184041473/bFyXy8Pu_400x400.jpeg" title="Svelter? I barely know her." style="
+"></img></center><br><center><i>"The best crutches I've ever used"</i></center><br><center>-Ligma</center><br></div><div class=" col1"><center><img src="https://pbs.twimg.com/profile_images/557940120184041473/bFyXy8Pu_400x400.jpeg" title="Svelter? I barely know her." style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
     width: 30vmin;
-"></img></center><br><center>I can't imagine programming without it </center><br><center>-Svelte</center><br></div></div>`;
+"></img></center><br><center><i>"I can't imagine programming without it"</i></center><br><center>-Svelte</center><br></div></div>`;
 
 globalThis.balls=`<script>
 
-globalThis.ligma=\`
+globalThis.ligma=\``+
 
-<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?</h2>
-<div class=" row"><div class=" col1"><center><img id="ligma-balls" src="https://static.frontendmasters.com/assets/teachers/theprimeagen/thumb.webp" title="Deez Nuts" style="
-    border-radius: 1vmax;
-    filter: drop-shadow(2px 4px 6px black);
-    height: 30vmin;
-    width: 30vmin;
-"></img></center><br><center>TypeScript is blazingly fast </center><br><center>-Nutflix</center><br></div><div class=" col1"><center><img src="https://create.t3.gg/images/theo_300x300.webp" title="ligma balls" style="
-    border-radius: 1vmax;
-    filter: drop-shadow(2px 4px 6px black);
-    height: 30vmin;
-    width: 30vmin;
-"></img></center><br><center>The best crutches I've ever used </center><br><center>-Ligma</center><br></div><div class=" col1"><center><img src="https://pbs.twimg.com/profile_images/557940120184041473/bFyXy8Pu_400x400.jpeg" title="Svelter? I barely know her." style="
-    border-radius: 1vmax;
-    filter: drop-shadow(2px 4px 6px black);
-    height: 30vmin;
-    width: 30vmin;
-"></img></center><br><center>I can't imagine programming without it </center><br><center>-Svelte</center><br></div></div>
+globalThis.ligma
 
-\`
++`\;
 
 setInterval(function(){
 if(document.querySelector('a[href*="most-loved-dreaded-and-wanted"]')){
