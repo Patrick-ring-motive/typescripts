cc442058ed3e2d5335e6ac0d28b00148c44f04bc
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-20 02:57:13 +0000

    <button id="cookie-accept">Accept       </button>

--- api/cookies.html.mjs
+++ api/cookies.html.mjs
@@ -17,9 +17,10 @@ globalThis.cooky=`
 
 <div id="onetrust-consent-sdk" style="opacity:1;">
   <div id="onetrust-banner-sdk" class="otFlat bottom ot-wo-title vertical-align-content ot-fade-in" role="region" aria-label="Cookie banner">
-    <p id="onetrust-policy-text">This site uses cookies and similar tracking technology. As disclosed in our Privacy Statement, we and our partners may collect personal information and other data. By continuing to use our website, you accept our  Privacy Statement and Terms of Service. 
-      <button id="cookie-accept">Accept
+    <p id="onetrust-policy-text">    <button id="cookie-accept">Accept
       </button>
+      This site uses cookies and similar tracking technology. As disclosed in our Privacy Statement, we and our partners may collect personal information and other data. By continuing to use our website, you accept our  Privacy Statement and Terms of Service. 
+  
     </p>
   </div>
 </div>
