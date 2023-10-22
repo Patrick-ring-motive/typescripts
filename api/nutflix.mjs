02b4c5e4910677c16b10db94ab291bc5e64e40c3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-22 02:26:14 +0000

    https://files-servleteer-vercel-app.vercel.app/typescripts/deez.webp

--- api/nutflix.mjs
+++ api/nutflix.mjs
@@ -3,17 +3,17 @@
 
 
 globalThis.ligma=`<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?</h2>
-<div class=" row"><div class=" col1"><center><img id="ligma-balls" src="https://static.frontendmasters.com/assets/teachers/theprimeagen/thumb.webp" title="Deez Nuts 🥜" style="
+<div class=" row"><div class=" col1"><center><img id="ligma-balls" src="https://files-servleteer-vercel-app.vercel.app/typescripts/deez.webp" title="Deez Nuts 🥜" style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
     width: 30vmin;
-"></img></center><br><center><i>"TypeScript is blazingly fast"</i></center><br><center>-Nutflix</center><br></div><div class=" col1"><center><img src="https://create.t3.gg/images/theo_300x300.webp" title="ligma balls 👅" style="
+"></img></center><br><center><i>"TypeScript is blazingly fast"</i></center><br><center>-Nutflix</center><br></div><div class=" col1"><center><img src="https://files-servleteer-vercel-app.vercel.app/typescripts/ligma.webp" title="ligma balls 👅" style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
     width: 30vmin;
-"></img></center><br><center><i>"The best crutches I've ever used"</i></center><br><center>-Ligma</center><br></div><div class=" col1"><center><img src="https://pbs.twimg.com/profile_images/557940120184041473/bFyXy8Pu_400x400.jpeg" title="Svelter? I barely know her. 🌽" style="
+"></img></center><br><center><i>"The best crutches I've ever used"</i></center><br><center>-Ligma</center><br></div><div class=" col1"><center><img src="https://files-servleteer-vercel-app.vercel.app/typescripts/svelte.jpeg" title="Svelter? I barely know her. 🌽" style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
     height: 30vmin;
