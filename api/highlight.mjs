efdb149ef512f15342518c9eb5d8f642eab1f0a0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-23 18:06:16 +0000

    asdf

--- api/highlight.mjs
+++ api/highlight.mjs
@@ -63,7 +63,7 @@ if(document.body){
     globalThis.Prism=Prism;Prism.highlightAll();
     
     let ss = document.createElement('style');
-    ss.innerHTML='code[class*="language-"], pre[class*="language-"]{color:blue;} code[class*="language-"] *:not(code,pre), pre[class*="language-"] *:not(code,pre){color:green;} .code-container{overflow:visible;} .line:not(:has(span:not([style="color: #008000"]))>span[style="color: #008000"]{text-wrap:pretty;}';
+    ss.innerHTML='code[class*="language-"], pre[class*="language-"]{color:blue;} code[class*="language-"] *:not(code,pre), pre[class*="language-"] *:not(code,pre){color:green;} pre div.code-container{overflow:visible;} .line:not(:has(span:not([style="color: #008000"]))>span[style="color: #008000"]{text-wrap:pretty;}';
     document.body.appendChild(ss);
     };
   document.body.appendChild(g); 
