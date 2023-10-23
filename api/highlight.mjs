c0f7a364c673d9d8c4200956a7a8c767fb47d5ac
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-23 16:24:30 +0000

    (document.body||document.firstElementChild)

--- api/highlight.mjs
+++ api/highlight.mjs
@@ -18,19 +18,19 @@ async function getp(){
 /*if((!window.location.href.includes('/dev/typescript-vfs'))
 &&(!window.location.href.includes('/dev/sandbox'))){return;}*/
   let thisLang = 'go';
-  let codes=document.querySelectorAll('code>pre:not([highlighted]),pre:not([highlighted]):has(code.html-code)');
+  let codes=document.querySelectorAll('code>pre:not([highlighted]),pre:not([highlighted]):has(code.html-code),pre:not([highlighted]):has(code):not(:has(.language-id))');
   let codes_length=codes.length;
   for(let i=0;i<codes_length;i++){
     codes[i].innerHTML='<code class="language-'+thisLang+'">'+codes[i].innerHTML+'</code>';
     codes[i].setAttribute('highlighted','true');
   }
-
+Prism.highlightAll();
   if(!document.querySelector('[id="prismmincss"]')){
   let l=document.createElement('link');
   l.href='https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css';
   l.rel='stylesheet';
   l.id="prismmincss";
-  document.body.appendChild(l);
+  (document.body||document.firstElementChild).appendChild(l);
   }
 
   if(!document.querySelector('[id="prismminjs"]')){
@@ -43,10 +43,10 @@ async function getp(){
     g.src='https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/prism-go.min.js';
     g.id="prismgominjs";
     g.onload=function(){Prism.highlightAll();};
-    document.body.appendChild(g); 
+    (document.body||document.firstElementChild).appendChild(g); 
     }  
   };
-  document.body.appendChild(m);
+  (document.body||document.firstElementChild).appendChild(m);
   }
 }
 </script>`;
