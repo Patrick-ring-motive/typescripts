337b56a3f28ef1e60337ddc708f418844b3d0b22
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-27 03:37:43 +0000

    vvvvvv

--- api/link-resolver-import.mjs
+++ api/link-resolver-import.mjs
@@ -43,7 +43,7 @@ setInterval(function(){
   transformLinks('action');
 
 if(!window.location.href.includes('play')){
-let anchors = document.querySelectorAll('a[href*="typescripts.org"]:not([clickable],.dropdown-toggle,[href*="/play"])');
+let anchors = document.querySelectorAll('a[href*="typescripts.org"]:not([clickable],.dropdown-toggle,[href*="/play"],[role="tab"])');
 let anchors_length = anchors.length;
 for(let i=0;i<anchors_length;i++){try{
 
