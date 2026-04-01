49651b7a333014a10968d98c501d77c1985d2a8d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 02:50:56 -0500

    style: js-beautify formatting (api/nutflix.mjs)

--- api/nutflix.mjs
+++ api/nutflix.mjs
@@ -1,8 +1,4 @@
-
-
-
-
-globalThis.ligma=`<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?</h2>
+globalThis.ligma = `<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?</h2>
 <div class=" row"><div class=" col1"><center><img id="ligma-balls" src="https://files-servleteer-vercel-app.vercel.app/typescripts/deez.webp" title="Deez Nuts 🥜" style="
     border-radius: 1vmax;
     filter: drop-shadow(2px 4px 6px black);
@@ -20,40 +16,43 @@ globalThis.ligma=`<h2 id="tst">What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍�
     width: 30vmin;
 "></img></center><br><center><i>"I can't imagine programming without it"</i></center><br><center>-Svelte</center><br></div></div>`;
 
-globalThis.balls=`<script>
+globalThis.balls = `<script>
 
-globalThis.ligma=\``+
+globalThis.ligma=\`` +
 
-globalThis.ligma
+  globalThis.ligma
 
-+`\`;
+  +
+  `\`;
 
-setInterval(`+
-function(){
-  (document?.querySelector?.('a[href*="most-loved-dreaded-and-wanted"]')?.parentElement??{}).innerHTML='TypeScript is truly one of the languages of all time.';
-if(document.querySelector('[id="ligma-balls"]')){return;}
-let b = document.querySelectorAll('h2');
-for(let i=0;i<b.length;i++){
-if(b[i].innerHTML=='What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?'){
-b[i].outerHTML=globalThis.ligma;
-return;
-}
-
-}
-
-  if(window.location.href.includes('basic-types')){
-
-let preamble=document.querySelector('.preamble:not([asdf])');
-  if(preamble){
-    let pa = document.createElement('a');
-    pa.target="_blank";
-    pa.href="https://www.reddit.com/r/programminghorror/s/F6oZg3Ne7q";
-    pa.innerHTML='<center><img src="https://i.redd.it/3rwrhktscmxb1.jpg" style="max-width:50vw;border-radius:1vmax;filter: drop-shadow(2px 4px 6px black);"></center>';
-      preamble.setAttribute('asdf','asdf');
-    preamble.appendChild(pa);
-  }
+setInterval(` +
+  function() {
+    (document?.querySelector?.('a[href*="most-loved-dreaded-and-wanted"]')?.parentElement ?? {}).innerHTML = 'TypeScript is truly one of the languages of all time.';
+    if (document.querySelector('[id="ligma-balls"]')) {
+      return;
     }
-  
-}+`,100);
+    let b = document.querySelectorAll('h2');
+    for (let i = 0; i < b.length; i++) {
+      if (b[i].innerHTML == 'What is 𝖳𝗒𝗉𝖾𝖲𝖼𝗋𝗂𝗉𝗍𝗌?') {
+        b[i].outerHTML = globalThis.ligma;
+        return;
+      }
+
+    }
+
+    if (window.location.href.includes('basic-types')) {
+
+      let preamble = document.querySelector('.preamble:not([asdf])');
+      if (preamble) {
+        let pa = document.createElement('a');
+        pa.target = "_blank";
+        pa.href = "https://www.reddit.com/r/programminghorror/s/F6oZg3Ne7q";
+        pa.innerHTML = '<center><img src="https://i.redd.it/3rwrhktscmxb1.jpg" style="max-width:50vw;border-radius:1vmax;filter: drop-shadow(2px 4px 6px black);"></center>';
+        preamble.setAttribute('asdf', 'asdf');
+        preamble.appendChild(pa);
+      }
+    }
+
+  } + `,100);
 
 </script>`;
